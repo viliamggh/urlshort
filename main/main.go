ecd44176525824a95a03f29106ba403fcf702de7
Viliam Gago <viliam.gago95@gmail.com>
2024-04-01 13:18:10 +0200

    add universal handler (json+yaml)

--- main/main.go
+++ main/main.go
@@ -1,38 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	urlshort "urlshort/urlpkg"
 )
 
 func main() {
 	mux := defaultMux()
 
-	// // Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-		"/car":            "https://www.sauto.cz/",
+	filePath := flag.String("file", "", "Path to the YAML file")
+	flag.Parse()
+	if *filePath == "" {
+		log.Fatal("You must specify a file path using the -file flag.")
 	}
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// Read the YAML file specified by the file path flag.
+	data, err := os.ReadFile(*filePath)
+
+	if err != nil {
+		log.Fatalf("Error reading file %s: %v", *filePath, err)
+	}
+
+	fileExtension := strings.ToLower(filepath.Ext(*filePath))
+
+	var parser urlshort.DataParser
+	switch fileExtension {
+	case ".yaml", ".yml":
+		parser = urlshort.YamlParser{}
+	case ".json":
+		parser = urlshort.JsonParser{}
+	default:
+		log.Fatalf("Unsupported file type: %s", fileExtension)
+	}
+
+	handler, err := urlshort.UniversalHandler(parser, data, mux)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Starting the server on :8080")
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", handler)
 }
 
 func defaultMux() *http.ServeMux {
--- urlpkg/handler.go
+++ urlpkg/handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"gopkg.in/yaml.v2"
@@ -10,6 +11,26 @@ import (
 type PathURLMapping struct {
 	Path string `yaml:"path"`
 	URL  string `yaml:"url"`
+}
+
+type DataParser interface {
+	Parse(data []byte) ([]PathURLMapping, error)
+}
+
+type YamlParser struct{}
+
+func (yp YamlParser) Parse(data []byte) ([]PathURLMapping, error) {
+	var mappings []PathURLMapping
+	err := yaml.Unmarshal(data, &mappings)
+	return mappings, err
+}
+
+type JsonParser struct{}
+
+func (jp JsonParser) Parse(data []byte) ([]PathURLMapping, error) {
+	var mappings []PathURLMapping
+	err := json.Unmarshal(data, &mappings)
+	return mappings, err
 }
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
@@ -26,26 +47,13 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-
-	parsedYaml, err := parseYAML(yml)
+func UniversalHandler(parser DataParser, data []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedData, err := parser.Parse(data)
 	if err != nil {
 		return nil, err
 	}
-
-	pathMap := buildMap(parsedYaml)
+	pathMap := buildMap(parsedData)
 	return MapHandler(pathMap, fallback), nil
-
-}
-
-func parseYAML(yml []byte) ([]PathURLMapping, error) {
-	var mappings []PathURLMapping
-	err := yaml.Unmarshal(yml, &mappings)
-	if err != nil {
-		return nil, err
-	}
-
-	return mappings, nil
 }
 
 func buildMap(mappings []PathURLMapping) map[string]string {
